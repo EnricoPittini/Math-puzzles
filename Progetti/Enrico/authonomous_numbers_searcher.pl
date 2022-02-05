e03b8e58279a217b93a83932caf4a7c247b9cf4d
Enrico Pittini <pittinienrico@hotmail.it>
2022-02-05 20:53:20 +0100

    Update authonomous_numbers_searcher.pl

--- Progetti/Enrico/authonomous_numbers_searcher.pl
+++ Progetti/Enrico/authonomous_numbers_searcher.pl
@@ -3,14 +3,39 @@
 authonomous_number(N, MinL, MaxL, Ds) :-
     %U is 10**18,
     Min is 10**(MinL-1),
-    Max is 10**(MaxL-1),
+    Max is 10**MaxL-1,
     N in Min..Max,
     
-    divisors(N, Ds),
+    %divisors(N, Ds),
     
     number_to_list(N, L),
     
-    even_length(L),
+    %non_zero_digits(L),
+    L ins 1..9,
+    
+    %even_length(L),
+    length(L, N1), N1 mod 2 #= 0,  
+    
+    ascending_order(L),
+    
+    digits_count(L),
+    
+    all_digits(L),
+    
+    divisors(N, Ds).
+    
+    %label([L]).
+
+% TODO : delete
+authonomous_number_LIST(L, MinL, MaxL, Ds) :-
+    %U is 10**18,
+    length(L, N),
+    N in MinL..MaxL,
+    N mod 2 #= 0,
+    
+    divisors_LIST(L, Ds),
+    
+    %even_length(L),
     
     %non_zero_digits(L),
     L ins 1..9,
@@ -30,6 +55,31 @@ divisors(N, [H|T]) :-
     H #<0,
     N mod abs(H) #> 0,
     divisors(N, T).
+
+% TODO : delete
+divisors_LIST(_, []).
+divisors_LIST(L, [D|T]) :-
+    D #= 11,
+    divisors_LIST_11(L),
+    divisors_LIST(L, T).
+divisors_LIST(L, [D|T]) :-
+    D #= 8,
+    divisors_LIST_8(L),
+    divisors_LIST(L, T).
+% TODO : delete
+divisors_LIST_8([H1,H2,H3]) :- 
+    (H1*100 + H2*10 + H3) mod 8 #= 0.
+divisors_LIST_8([_|T]) :- divisors_LIST_8(T).
+% TODO : delete
+divisors_LIST_11(L) :- sum_even(L,N1), sum_odd(L,N2), abs(N1-N2) mod 11 #= 0.
+sum_odd([], 0).
+sum_odd([H1,_|T], N) :-
+    sum_odd(T, N1),
+    N #= N1+H1.
+sum_even([], 0).
+sum_even([_,H2|T], N) :-
+    sum_odd(T, N1),
+    N #= N1+H2.
 
 number_to_list(N, [N]) :-
     N//10 #= 0.
@@ -52,11 +102,12 @@ append([H1|T1],L2,[H1|L]) :-
     % The result of the concatenation of the whole two lists is [H1|L], where H1 is the head of
     % the first list.
 
+% TODO : delete
 even_length([]).
 even_length([_,_|T]) :-
     even_length(T).
     
-
+% TODO : delete
 non_zero_digits([]).
 non_zero_digits([H|T]) :-
     H #\= 0,
